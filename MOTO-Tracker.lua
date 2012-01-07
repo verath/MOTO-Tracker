751cc9d0df841610eb33da667f3a9d6e16482ff7
Peter Eliasson <warackta@gmail.com>
2012-01-07 17:16:53 +0100

    Started adding tracking for guild roster changes.

--- MOTO-Tracker.lua
+++ MOTO-Tracker.lua
@@ -19,10 +19,6 @@ local L,A,I = MOTOTracker.locale, MOTOTracker.addon, MOTOTracker.info
 --   Helper Functions
 --###################################
 
-local function UpdateGuildRoster()
-	
-end
-
 function A:SetupDB()
 	self:SetupDefaults()
 	self.db = LibStub("AceDB-3.0"):New("MOTOTrackerDB", A.defaults, true)
@@ -45,13 +41,42 @@ function A:OnEnable()
 		A:Print(L['MOTO Tracker enabled.'])
 	end
 
-	I.hasGuild = IsInGuild()
+	-- Start listening for events
+	A:RegisterEvent('GUILD_ROSTER_UPDATE', 'OnGuildRosterUpdate')
+	
+	-- Request guild roster from server
+	GuildRoster()
 
+	-- Static values
+	I.hasGuild = IsInGuild()
 	if I.hasGuild then
 		I.guildName, _ = GetGuildInfo("player")
 	end
 
+	-- Set up the options UI
 	self:SetupOptions()
 end
 
+-- Gets called if the addon is disabled
+function A:OnDisable()
 
+	-- Unregister Events
+	A:UnregisterEvent('GUILD_ROSTER_UPDATE')
+end
+
+
+-- When guild roster is updated
+local firstRosterUpdate = true
+function A:OnGuildRosterUpdate( event, change )
+	if not I.hasGuild then return end
+
+	-- We only need to update our DB if a change did occur
+	-- or if this is first update after login.
+	if change == nil and firstRosterUpdate ~= true then return end
+	firstRosterUpdate = false
+
+	local numGuildMembers, _ = GetNumGuildMembers()
+	A:Print(numGuildMembers)
+	A:Print(change)
+
+end
